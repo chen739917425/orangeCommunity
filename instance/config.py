6228c0cc74e44713bf9192158756f6ee79545011
orange <739917425@qq.com>
2019-12-01 12:05:43 +0800

    docs: 更新.gitignore

---
+++ instance/config.py
@@ -0,0 +1,8 @@
+HOST = '127.0.0.1'
+PORT = 3306
+USER = 'root'
+PASSWD = 'orange'
+DB = 'orange_community'
+CHARSET = 'utf8'
+SECRET_KEY = b'W\xf9\xe4f+s\xa8z@\xb8{YJ\x86\xbd\xad'
+SALT = '8sh+34=hd8_39dh-e7yqcn'
